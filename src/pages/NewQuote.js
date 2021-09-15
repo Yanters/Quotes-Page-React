072914c724451252adfaf531d848863e8b3727c4
43695512 <yanter@wp.pl>
2021-09-15 21:56:52 +0200

    Sorting Quotes, redirecting after creating a quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.replace("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
